6e5cc36114fcab7585eb5e43bc18571bacbaee8c
Rafael Costa <101342822+RafaelPinheiroCosta@users.noreply.github.com>
2023-05-02 15:32:09 -0300

    Atualização 05 de Maio

--- app/src/main/java/com/example/dado/MainActivity.kt
+++ app/src/main/java/com/example/dado/MainActivity.kt
@@ -12,6 +12,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -37,7 +41,17 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun RolaDado(){
-    var resultado = 1
+    var resultado by remember { mutableStateOf(1) }
+
+    val imageResource = when (resultado) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -45,7 +59,7 @@ fun RolaDado(){
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-            painter = painterResource(id = R.drawable.dice_1),
+            painter = painterResource(id = imageResource),
             contentDescription = null
         )
         Button(
